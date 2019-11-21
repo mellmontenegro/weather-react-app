fcbb77514154756c1346c120e399045638a82b0c
mellmontenegro <56005764+mellmontenegro@users.noreply.github.com>
2019-11-21 20:33:35 +0000

    Created function on Temperature component

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Windows!</p>
         <Loader type="Puff" color="#09d3ac" height={50} width={50} />
-        <Temperature city="BH" />
-        <a
-          className="App-link"
-          href="https://github.com/mellmontenegro/weather-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open Source Code
-        </a>{" "}
-        by Mariana Almeida
+        <Temperature city="Belo Horizonte" />
+            Open Source Code
+          </a>{" "}
+          by Mariana Almeida
+        </small>
       </header>
     </div>
   );
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,5 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 export default function Temperature(props) {
-  return <h1>The temperature in {props.city} is 16ºC</h1>;
+  function showTemperature(response) {
+    let apiKey = "875ea8327db11c913ffcc92c93a23805";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
+  }
+  return (
+    <h1>
+      The temperature in {props.city} is {response.data.main.temp}ºC
+    </h1>
+  );
 }
