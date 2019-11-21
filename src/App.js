e73cb9d097aca23169b2351f5f90eb58dbe38147
mellmontenegro <56005764+mellmontenegro@users.noreply.github.com>
2019-11-21 19:15:50 +0000

    Created link for gitHub

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello Windows!</p>
+        <a
+          className="App-link"
+          href="https://github.com/mellmontenegro/weather-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Source Code
+        </a>{" "}
+        by Mariana Almeida
       </header>
     </div>
   );
