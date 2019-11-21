89d00d2afba917fb07875c683950fc5f3c48646c
mellmontenegro <56005764+mellmontenegro@users.noreply.github.com>
2019-11-21 19:28:40 +0000

    Added loader dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Windows!</p>
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
         <a
           className="App-link"
           href="https://github.com/mellmontenegro/weather-react-app"
